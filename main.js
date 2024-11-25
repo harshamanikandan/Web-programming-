18dade83f175ad5880b200a9b6d7afc12e829067
harshamanikandan <147386375+harshamanikandan@users.noreply.github.com>
2024-11-25 15:36:16 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".client__rating", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+
+ScrollReveal().reveal(".download__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".download__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".download__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".download__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".download__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
